cd5af0e5b05002a4dc20afe3f62e40ff1328cdcd
felix919 <panmatsu@gmail.com>
2025-01-29 21:18:51 +0900

    Initial commit

---
+++ Gakuen_tips/Gakuen_tips/Gakuen_tipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gakuen_tipsApp.swift
+//  Gakuen_tips
+//
+//  Created by 松広登 on 2025/01/29.
+//
+
+import SwiftUI
+
+@main
+struct Gakuen_tipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
